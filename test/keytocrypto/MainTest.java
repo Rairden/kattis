3597b4dcacedf7d8b472a7cc5248b4a22f3c2494
erik <erik.rairden@gmail.com>
2019-05-22 00:02:02 -0500

    fixed test

--- test/keytocrypto/MainTest.java
+++ test/keytocrypto/MainTest.java
@@ -9,8 +9,8 @@ class MainTest {
     @Test
     void previous() {
         // B - N = O    1 - 13 = 14
-        int result = Main.previous(25, 1, 13);
-        assertEquals(14, result);
+        int result1 = Main.previous(25, 1, 13);
+        assertEquals(14, result1);
 
         // A - O = M    0 - 14 = 12
         int result2 = Main.previous(25, 0, 14);
@@ -18,6 +18,6 @@ class MainTest {
 
         // Y - Z = Z    24 - 25 = 25
         int result3 = Main.previous(25, 24, 25);
-        assertEquals(25, Main.previous(25, 24, 25));
+        assertEquals(25, result3);
     }
 }

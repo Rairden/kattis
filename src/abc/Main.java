64de12fedb8501230c750723eafc43a55bcc9525
erik <erik.rairden@gmail.com>
2019-05-22 02:59:45 -0500

    Scanner .nextLine() not working. Doesn't skip empty string.

--- src/abc/Main.java
+++ src/abc/Main.java
@@ -5,46 +5,63 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String input = "1 5 3 \n ABC";
+        String input = "1 5 3\nABC";
         Scanner scan = new Scanner(input);
-        mySort(2,1,3);
+        int[] arr = mySort(scan.nextInt(), scan.nextInt(), scan.nextInt());
+
+        String command = scan.nextLine();
+
+        for (int i = 0; i < 3; i++) {
+            switch (command) {
+                case "A":
+                    System.out.print(arr[0]);
+                case "B":
+                    System.out.print(arr[1]);
+                case "C":
+                    System.out.print(arr[2]);
+            }
+        }
     }
 
     private static int[] mySort(int a, int b, int c) {
-        int min = 0;
-        int mid = 0;
-        int max = 0;
-        int[] arr = new int[]{0,0,0};
+        int min;
+        int mid;
+        int max;
+        int[] arr1 = new int[]{a, b, c};
 
         if (a < b || a < c) {
             if (b < c) {
-                min = a;
-                mid = b;
                 max = c;
-            }
-            else if ( b > c) {
+                if (a < b) {
+                    min = a;
+                    mid = b;
+
+                } else {
+                    min = b;
+                    mid = a;
+                }
+            } else if (b > c && a < c) {
                 min = a;
                 mid = c;
                 max = b;
+            } else {
+                min = c;
+                mid = a;
+                max = b;
             }
-        } else if (a > b || a > c) {
-            if (b > c)
-                max = c;
-            else if (b < c) {
+        } else {
+            max = a;
+            if (b < c) {
                 min = b;
                 mid = c;
-                max = a;
             } else {
                 min = c;
                 mid = b;
-                max = a;
             }
         }
-
-        arr[0] = min;
-        arr[1] = mid;
-        arr[2] = max;
-        return arr;
+        arr1[0] = min;
+        arr1[1] = mid;
+        arr1[2] = max;
+        return arr1;
     }
-
 }

77e018d7a4b337c775b13895850eaaad4c2ce636
erik <erik.rairden@gmail.com>
2019-05-23 04:09:10 -0500

    completed veci final, 2 files

---
+++ src/veci/IntegerToList.java
@@ -0,0 +1,44 @@
+package veci;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class IntegerToList {
+    public List<Integer> firstNum;
+    public List<Integer> secondNum;
+    private Stack<Integer> stack;
+    public int zero, one, two, three, four, five, six, seven, eight, nine;
+
+    static IntegerToList convert = null;
+
+    private IntegerToList() {
+        firstNum    = new ArrayList<>();
+        secondNum   = new ArrayList<>();
+        stack       = new Stack<>();
+        int zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
+    }
+
+    public static IntegerToList getInstance() {
+        if (convert == null) {
+            convert = new IntegerToList();
+        }
+        return convert;
+    }
+
+    /**
+     * @param number the number we want to split apart to add to a List
+     */
+    public static void splitNumber(int number) {
+        while (number > 0) {
+            int remainder = number % 10;
+            convert.stack.push(remainder);
+            number /= 10;
+        }
+
+        for (Iterator<Integer> itr = convert.stack.iterator(); itr.hasNext(); ) {
+            convert.secondNum.add(convert.stack.pop());
+        }
+    }
+}

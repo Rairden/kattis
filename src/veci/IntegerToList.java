eb87f9974d8b072f504ea9203f77036aeaa225ad
erik <erik.rairden@gmail.com>
2019-05-23 04:17:48 -0500

    completed veci final, 2 files

--- src/veci/IntegerToList.java
+++ src/veci/IntegerToList.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class IntegerToList {
+
     public List<Integer> firstNum;
     public List<Integer> secondNum;
     private Stack<Integer> stack;

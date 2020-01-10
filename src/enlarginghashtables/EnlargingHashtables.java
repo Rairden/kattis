bf3f837dcc2384369228b17157e7f949eda90513
Rairden <erik.rairden@gmail.com>
2020-01-10 16:58:23 -0600

    completed enlarginghashtables

---
+++ src/enlarginghashtables/EnlargingHashtables.java
@@ -0,0 +1,40 @@
+package enlarginghashtables;
+
+import lib.io.FastReader;
+
+import static lib.math.MyMath.isPrime;
+
+public class EnlargingHashtables {
+
+    public static void main(String[] args) {
+        String in = """
+                29
+                33
+                0""";
+        FastReader io = new FastReader(in);
+
+        while (true) {
+            int x = io.nextInt();
+            if (x == 0) {
+                break;
+            }
+            resizeHashTable(x);
+        }
+    }
+
+    static void resizeHashTable(int tableSize) {
+        int doubled = tableSize * 2 + 1;
+        while (true) {
+            if (isPrime(doubled)) {
+                if (!isPrime(tableSize)) {
+                    System.out.printf("%d (%d is not prime)\n", doubled, tableSize);
+                } else {
+                    System.out.println(doubled);
+                }
+                return;
+            } else {
+                doubled += 2;
+            }
+        }
+    }
+}
